6c63f7779e09dcb9b7695d9754b4698a1ca5c655
Nick <71181253+nicktor19@users.noreply.github.com>
2022-03-07 22:35:06 -0500

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,13 @@
+<?php 
+    // auto loader:
+    require_once("includes/auto_classloader.inc.php");
+
+    ?>
+    <fieldset>
+        <legend>Basic Calculator:</legend>
+    <?php
+
+    $cal = new Calculator;
+    unset($cal);
+?>
+</fieldset>
